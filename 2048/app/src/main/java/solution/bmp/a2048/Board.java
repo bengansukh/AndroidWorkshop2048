af04ec1a12a60cec1e6f710981859f4b68ea0407
Ben Gansukh <gansukhben@gmail.com>
2017-02-18 15:48:40 -0600

    Adding board

--- 2048/app/src/main/java/solution/bmp/a2048/Board.java
+++ 2048/app/src/main/java/solution/bmp/a2048/Board.java
@@ -1,5 +1,7 @@
 package solution.bmp.a2048;
 
+import java.util.Random;
+
 /**
  * Created by gansu on 2/18/2017.
  */
@@ -15,6 +17,25 @@ public class Board {
     public Board(int size){
         this.size = size;
         this.board = new Cell[size][size];
+        for( int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                board[i][j] = new Cell();
+            }
+        }
+        Random rand = new Random();
+        int firstI = rand.nextInt(4);
+        int firstJ = rand.nextInt(4);
+
+        board[firstI][firstJ].random();
+        int secondI = rand.nextInt(4);
+        int secondJ = rand.nextInt(4);
+        while(firstI == secondI && firstJ == secondJ)
+        {
+            secondI = rand.nextInt(4);
+            secondJ = rand.nextInt(4);
+        }
+        board[secondI][secondJ].random();
+
         this.score = 0;
     }
 
@@ -27,13 +48,30 @@ public class Board {
     }
 
     public void moveRight(){
-        for( int i = 0; i < size; i++){
-            for(int j = 0; j < size; j++){
-
+        int moved = 0;
+        for( int j = 0; j < size; j++){
+            for(int i = 0; i < size; i++){
+                if(!swapCells(board[i][j], board[i][j+1]))
+                {
+                    moved++;
+                }
             }
         }
+
+        if(moved == 0)
+            return;
+        moveRight();
     }
 
+    private boolean swapCells(Cell movingCell, Cell emptyCell)
+    {
+        if(emptyCell.getValue() != 0)
+            return false;
+        Cell temp = movingCell;
+        movingCell = emptyCell;
+        emptyCell = temp;
+        return true;
+    }
     public Cell[][] getBoard(){
         return this.board;
     }
